3ab51d7160898335e797d22d7a5edfae93654451
nishikawa <Ryosuke.Nishikawa@jp.ricoh.com>
2020-12-07 14:51:07 +0900

    interface名の変更

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ImageArea } from './ImageArea';
 import './index.css';
 import { Util } from './Util';
 import { Error } from './Error';
@@ -9,10 +8,10 @@ import { Image } from './types';
 import { ErrorId } from './types';
 import { Home } from './Home'
 
-interface HomeState {
+interface IndexState {
   inputUrl: string; // フォームに入力されたurl
   images: Array<Image> // 表示している画像のリスト
-  checkedImages: Array<boolean>
+  checkedImages: Array<boolean> // 画像のチェック状態
   errorId: number; // 1:画像が存在しない, 2:分析結果が取得できない, 0:エラーなし
   displayId: number; // 1:ホーム画面, 2:分析結果画面, 0:エラー画面
 }
@@ -23,7 +22,7 @@ enum DisplayId {
   ERROR
 }
 
-export default class Index extends React.Component<{}, HomeState> {
+export default class Index extends React.Component<{}, IndexState> {
   constructor(props: {}) {
     super(props);
     this.state = {
