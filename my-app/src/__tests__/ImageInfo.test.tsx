77714e9275155303efd420a58f21772430991a53
nishikawa <Ryosuke.Nishikawa@jp.ricoh.com>
2020-12-10 14:27:42 +0900

    ImageInfoクラスの関数のテスト追加

--- my-app/src/__tests__/ImageInfo.test.tsx
+++ my-app/src/__tests__/ImageInfo.test.tsx
@@ -4,28 +4,49 @@ import React from "react";
 import { Image } from "../types";
 
 
-const img: Image = {id: 1, url: "https://nishikawa.blob.core.windows.net/images/steve/2020/10/15/01.jpg?sv=2019-07-07&sr=c&si=myPolicyPS&sig=FkKJ4nXCiqzDYjbSaDfqli%2FnErPRTKrD%2BUQfH0MT3ac%3D"};
+const img: Image = { id: 1, url: "https://nishikawa.blob.core.windows.net/images/steve/2020/10/15/01.jpg?sv=2019-07-07&sr=c&si=myPolicyPS&sig=FkKJ4nXCiqzDYjbSaDfqli%2FnErPRTKrD%2BUQfH0MT3ac%3D" };
 
-it("unchecked", () => {
-    const tree = create(
-        <ImageInfo
-            image={img}
-            onClick={() => { }}
-            checked={false}
-        />
-    ).toJSON();
-    expect(tree).toMatchSnapshot();
+describe("checked test", () => {
+    it("unchecked", () => {
+        const tree = create(
+            <ImageInfo
+                image={img}
+                onClick={() => { }}
+                checked={false}
+            />
+        ).toJSON();
+        expect(tree).toMatchSnapshot();
 
+    })
+
+    it("checked", () => {
+        const tree = create(
+            <ImageInfo
+                image={img}
+                onClick={() => { }}
+                checked={true}
+            />
+        ).toJSON();
+        expect(tree).toMatchSnapshot();
+
+    })
 })
 
-it("checked", () => {
-    const tree = create(
-        <ImageInfo
-            image={img}
-            onClick={() => { }}
-            checked={true}
-        />
-    ).toJSON();
-    expect(tree).toMatchSnapshot();
+describe("funcotino test", () => {
+    const props = {
+        image: img,
+        checked: false,
+        onClick: () => { },
+    };
+    const imageInfo = new ImageInfo(props);
 
+    it("should get name steve", () => {
+        const name = imageInfo.getName(img.url);
+        expect(name).toEqual("steve")
+    })
+
+    it("should get date 2020/10/15", () => {
+        const date = imageInfo.getDate(img.url);
+        expect(date).toEqual("2020/10/15")
+    })
 })
