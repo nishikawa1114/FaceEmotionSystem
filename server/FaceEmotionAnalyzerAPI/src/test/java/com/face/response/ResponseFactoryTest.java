d86b301fe80ba86aa4bc0e7454c82b422b6d72ab
nishikawa <Ryosuke.Nishikawa@jp.ricoh.com>
2021-01-29 11:40:05 +0900

    単体テスト　コメント修正

--- server/FaceEmotionAnalyzerAPI/src/test/java/com/face/response/ResponseFactoryTest.java
+++ server/FaceEmotionAnalyzerAPI/src/test/java/com/face/response/ResponseFactoryTest.java
@@ -22,7 +22,7 @@ class ResponseFactoryTest {
 	@Autowired
 	ResponseFactory target;
 
-	// レスポンス {"error":"test error"} を期待
+	// ErrorResponseが生成され、そのerrorの値が"test error"であることを期待
 	@Test
 	void testCreateErrorResponse() {
 		
@@ -35,11 +35,9 @@ class ResponseFactoryTest {
 		assertThat(response.getError()).isEqualTo(expectedErrorMessage);
 	}
 
-
-
-	// レスポンス {"error":"Face API response is error.",
-	//          "detail":{"code":"BadArgument",
-	//                    "message":"Request body is invalid."}} を期待
+	// FaceApiErrorResponseが生成され、errorに固定のエラー文、detailsにJSON文字列で指定された(実際には、FaceAPIから返却された)エラーの内容が設定されていることを期待。
+	// FaceApiErrorResponse.errorは固定のエラー文"Face API response is error."
+	// FaceApiErrorResponse.detailのcode, messageにSON文字列で指定されたエラーの内容が格納
 	@Test
 	void testCreateFaceApiErrorResponse() throws JsonMappingException, JsonProcessingException {
 		
