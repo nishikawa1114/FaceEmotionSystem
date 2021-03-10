e500ddf1798d1c7f78c13656b2f9c0068925872e
nishikawa <Ryosuke.Nishikawa@jp.ricoh.com>
2021-03-10 13:12:04 +0900

    API 単体テスト 平均値返却

--- server/FaceEmotionAnalyzerAPI/src/test/java/com/face/controller/FaceEmotionControllerTest.java
+++ server/FaceEmotionAnalyzerAPI/src/test/java/com/face/controller/FaceEmotionControllerTest.java
@@ -3,7 +3,7 @@ package com.face.controller;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
@@ -40,35 +40,43 @@ class FaceEmotionControllerTest {
 	@Test
 	void testAnalyzSuccess() throws Exception {
 
-		// 返却されるレスポンス
+		// FaceAPIからのレスポンス
 		String jsonResponseBody = "[{\"faceId\":\"580a572a-c529-4333-b93f-8429ebda8b18\",\"faceRectangle\":{\"top\":128,\"left\":529,\"width\":109,\"height\":109},\"faceAttributes\":{\"emotion\":{\"anger\":0.0,\"contempt\":0.0,\"disgust\":0.0,\"fear\":0.0,\"happiness\":1.0,\"neutral\":0.0,\"sadness\":0.0,\"surprise\":0.0}}}]";
+		// 期待値
+		String expect = "{\"total\":1,\"mean\":{\"faceAttributes\":{\"emotion\":{\"neutral\":0.0,\"anger\":0.0,\"contempt\":0.0,\"disgust\":0.0,\"fear\":0.0,\"happiness\":1.0,\"sadness\":0.0,\"surprise\":0.0}}},\"resultData\":[{\"faceId\":\"580a572a-c529-4333-b93f-8429ebda8b18\",\"faceRectangle\":{\"top\":128,\"left\":529,\"width\":109,\"height\":109},\"faceAttributes\":{\"emotion\":{\"neutral\":0.0,\"anger\":0.0,\"contempt\":0.0,\"disgust\":0.0,\"fear\":0.0,\"happiness\":1.0,\"sadness\":0.0,\"surprise\":0.0}}}]}";
 		// FaceAPI通信のモック
 		MockRestServiceServer mockServer = MockRestServiceServer.bindTo(restTemplate).ignoreExpectOrder(true)
 				.bufferContent().build();
-		mockServer.expect(method(HttpMethod.POST)).andRespond(withSuccess(jsonResponseBody, MediaType.APPLICATION_JSON));
-
+		mockServer.expect(method(HttpMethod.POST))
+				.andRespond(withSuccess(jsonResponseBody, MediaType.APPLICATION_JSON));
+		// 実行・比較
 		mockMvc.perform(MockMvcRequestBuilders.post("/face/emotion").contentType(MediaType.APPLICATION_JSON)
 				.content("{\"url\":\"https://nishikawa.blob.core.windows.net/images/sugimoto/2020/11/01/01.jpg\"}"))
-				.andDo(print())
+				.andExpect(content().json(expect))
 				.andExpect(status().isOk());
 	}
-	
+
 	// 成功時のテスト 画像に複数人移っている場合
 	// レスポンス HTTPステータス200を期待
 	@Test
 	void testAnalyzSuccess2() throws Exception {
 
-		// 返却されるレスポンス
+		// FaceAPIからのレスポンス
 		String jsonResponseBody = "[{\"faceId\":\"580a572a-c529-4333-b93f-8429ebda8b18\",\"faceRectangle\":{\"top\":128,\"left\":529,\"width\":109,\"height\":109},\"faceAttributes\":{\"emotion\":{\"anger\":0.0,\"contempt\":0.0,\"disgust\":0.0,\"fear\":0.0,\"happiness\":1.0,\"neutral\":0.0,\"sadness\":0.0,\"surprise\":0.0}}},"
-								+ "{\"faceId\":\"580a572a-c529-4333-b93f-8429ebda8b18\",\"faceRectangle\":{\"top\":128,\"left\":529,\"width\":109,\"height\":109},\"faceAttributes\":{\"emotion\":{\"anger\":0.0,\"contempt\":0.0,\"disgust\":0.0,\"fear\":0.0,\"happiness\":1.0,\"neutral\":0.0,\"sadness\":0.0,\"surprise\":0.0}}}]";
+				+ "{\"faceId\":\"580a572a-c529-4333-b93f-8429ebda8b18\",\"faceRectangle\":{\"top\":128,\"left\":529,\"width\":109,\"height\":109},\"faceAttributes\":{\"emotion\":{\"anger\":0.0,\"contempt\":0.0,\"disgust\":0.0,\"fear\":0.0,\"happiness\":1.0,\"neutral\":0.0,\"sadness\":0.0,\"surprise\":0.0}}}]";
+		// 期待値
+		String expect = "{\"total\":2,\"mean\":{\"faceAttributes\":{\"emotion\":{\"neutral\":0.0,\"anger\":0.0,\"contempt\":0.0,\"disgust\":0.0,\"fear\":0.0,\"happiness\":1.0,\"sadness\":0.0,\"surprise\":0.0}}},"
+				+ "\"resultData\":[{\"faceId\":\"580a572a-c529-4333-b93f-8429ebda8b18\",\"faceRectangle\":{\"top\":128,\"left\":529,\"width\":109,\"height\":109},\"faceAttributes\":{\"emotion\":{\"anger\":0.0,\"contempt\":0.0,\"disgust\":0.0,\"fear\":0.0,\"happiness\":1.0,\"neutral\":0.0,\"sadness\":0.0,\"surprise\":0.0}}},"
+				+ "{\"faceId\":\"580a572a-c529-4333-b93f-8429ebda8b18\",\"faceRectangle\":{\"top\":128,\"left\":529,\"width\":109,\"height\":109},\"faceAttributes\":{\"emotion\":{\"anger\":0.0,\"contempt\":0.0,\"disgust\":0.0,\"fear\":0.0,\"happiness\":1.0,\"neutral\":0.0,\"sadness\":0.0,\"surprise\":0.0}}}]}";
 		// FaceAPI通信のモック
 		MockRestServiceServer mockServer = MockRestServiceServer.bindTo(restTemplate).ignoreExpectOrder(true)
 				.bufferContent().build();
-		mockServer.expect(method(HttpMethod.POST)).andRespond(withSuccess(jsonResponseBody, MediaType.APPLICATION_JSON));
-
+		mockServer.expect(method(HttpMethod.POST))
+				.andRespond(withSuccess(jsonResponseBody, MediaType.APPLICATION_JSON));
+		// 実行・比較
 		mockMvc.perform(MockMvcRequestBuilders.post("/face/emotion").contentType(MediaType.APPLICATION_JSON)
 				.content("{\"url\":\"https://nishikawa.blob.core.windows.net/images/sugimoto/2020/11/01/01.jpg\"}"))
-				.andDo(print())
+				.andExpect(content().json(expect))
 				.andExpect(status().isOk());
 	}
 
@@ -76,45 +84,55 @@ class FaceEmotionControllerTest {
 	// レスポンス HTTPステータス400を期待
 	@Test
 	void testAnalyzeErrorNotDetected() throws Exception {
-		// 返却されるレスポンス
+		// FaceAPIからのレスポンス
 		String jsonResponseBody = "";
+		// 期待値
+		String expect = "{\"error\":\"face not detected.\"}";
 		// FaceAPI通信のモック
 		MockRestServiceServer mockServer = MockRestServiceServer.bindTo(restTemplate).ignoreExpectOrder(true)
 				.bufferContent().build();
 		mockServer.expect(method(HttpMethod.POST)).andRespond(withSuccess().body(jsonResponseBody));
-
-		MvcResult  result = mockMvc.perform(MockMvcRequestBuilders.post("/face/emotion").contentType(MediaType.APPLICATION_JSON)
+		// 実行・比較
+		mockMvc.perform(MockMvcRequestBuilders.post("/face/emotion").contentType(MediaType.APPLICATION_JSON)
 				.content("{\"url\":\"https://nishikawa.blob.core.windows.net/images/sugimoto/2020/11/01/01.jpg\"}"))
-				.andDo(print())
 				.andExpect(status().isBadRequest())
-				.andReturn();         
+				.andExpect(content().json(expect));
 	}
 
 	// POSTパラメータが不正の場合のテスト キー "url" → "uri"
 	// レスポンス HTTPステータス400を期待
+	// ※FaceAPIとの通信は行わない
 	@Test
 	void testAnalyzeErrorinvalidBody() throws Exception {
+		// 期待値
+		String expect = "{\"error\":\"request body is invalid.\"}";
+		// 実行・比較
 		mockMvc.perform(MockMvcRequestBuilders.post("/face/emotion").contentType(MediaType.APPLICATION_JSON)
 				.content("{\"uri\":\"https://nishikawa.blob.core.windows.net/images/sugimoto/2020/11/01/02.jpg\"}"))
-				.andDo(print())
+				.andExpect(content().json(expect))
 				.andExpect(status().isBadRequest());
 	}
 
 	// POSTパラメータが未入力の場合のテスト
 	// レスポンス HTTPステータス400を期待
+	// ※FaceAPIとの通信は行わない
 	@Test
 	void testAnalyzeNoEnterdBody() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.post("/face/emotion").contentType(MediaType.APPLICATION_JSON))
-				.andDo(print())
+//				.andExpect(content().json(expect))
 				.andExpect(status().isBadRequest());
 	}
 
 	// メディアタイプが不正の場合のテスト
 	// レスポンス HTTPステータス415を期待
+	// ※FaceAPIとの通信は行わない
 	@Test
 	void testAnalyzeErrorInvalidMediaType() throws Exception {
+		// 期待値
+		String expect = "{\"error\":\"media type is invalid.\"}";
+		// 実行・比較
 		mockMvc.perform(MockMvcRequestBuilders.post("/face/emotion").contentType(MediaType.APPLICATION_PDF))
-				.andDo(print())
+				.andExpect(content().json(expect))
 				.andExpect(status().isUnsupportedMediaType());
 	}
 
@@ -122,20 +140,21 @@ class FaceEmotionControllerTest {
 	// レスポンス HTTPステータス400を期待
 	@Test
 	void testAnalyzeErrorReturn400() throws Exception {
-
-		// 返却されるレスポンス
+		// FaceAPIからのレスポンス
 		String jsonResponseBody = "{ \"error\": { \"code\": \"BadArgument\", \"message\": \"Request body is invalid.\"} }";
+		// 期待値
+		String expect = "{\"error\":\"Face API response is error.\",\"details\":{\"code\":\"BadArgument\",\"message\":\"Request body is invalid.\"}}";
 		// FaceAPI通信のモック
 		MockRestServiceServer mockServer = MockRestServiceServer.bindTo(restTemplate).ignoreExpectOrder(true)
 				.bufferContent().build();
 		mockServer.expect(method(HttpMethod.POST))
 				.andRespond(withStatus(HttpStatus.BAD_REQUEST).body(jsonResponseBody));
-		// 実行
-		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/face/emotion").contentType(MediaType.APPLICATION_JSON)
-				.content("{\"url\":\"https://nishikawa.blob.core.windows.net/images/sugimoto/2020/11/01/01.jpg\"}"))
-				.andDo(print())
-				.andExpect(status().isBadRequest())
-				.andReturn();
+		// 実行・比較
+		MvcResult result = mockMvc
+				.perform(MockMvcRequestBuilders.post("/face/emotion").contentType(MediaType.APPLICATION_JSON).content(
+						"{\"url\":\"https://nishikawa.blob.core.windows.net/images/sugimoto/2020/11/01/01.jpg\"}"))
+				.andExpect(content().json(expect))
+				.andExpect(status().isBadRequest()).andReturn();
 
 		String content = result.getResponse().getContentAsString();
 		mockServer.verify();
@@ -145,38 +164,38 @@ class FaceEmotionControllerTest {
 	// レスポンス HTTPステータス400を期待
 	@Test
 	void testAnalyzeReturn429() throws Exception {
-
-		// 返却されるレスポンス
+		// FaceAPIからのレスポンス
 		String jsonResponseBody = "{ \"error\": { \"code\": \"BadArgument\", \"message\": \"Request body is invalid.\"} }";
+		// 期待値
+		String expect = "{\"error\":\"Face API response is error.\",\"details\":{\"code\":\"BadArgument\",\"message\":\"Request body is invalid.\"}}";
 		// FaceAPI通信のモック
 		MockRestServiceServer mockServer = MockRestServiceServer.bindTo(restTemplate).ignoreExpectOrder(true)
 				.bufferContent().build();
 		mockServer.expect(method(HttpMethod.POST))
 				.andRespond(withStatus(HttpStatus.TOO_MANY_REQUESTS).body(jsonResponseBody));
-		// 実行
+		// 実行・比較
 		mockMvc.perform(MockMvcRequestBuilders.post("/face/emotion").contentType(MediaType.APPLICATION_JSON)
 				.content("{\"url\":\"https://nishikawa.blob.core.windows.net/images/sugimoto/2020/11/01/01.jpg\"}"))
-				.andDo(print())
+				.andExpect(content().json(expect))
 				.andExpect(status().isBadRequest());
-
-		mockServer.verify();
 	}
 
 	// FaceAPIからHTTPステータス403(400,429以外)が返却される場合
 	// レスポンス HTTPステータス500を期待
 	@Test
 	void testAnalyzeReturn403() throws Exception {
-
-		// 返却されるレスポンス
+		// FaceAPIからのレスポンス
 		String jsonResponseBody = "{ \"error\": { \"code\": \"BadArgument\", \"message\": \"Request body is invalid.\"} }";
+		// 期待値
+		String expect = "{\"error\":\"Face API response is error.\",\"details\":{\"code\":\"BadArgument\",\"message\":\"Request body is invalid.\"}}";
 		// FaceAPI通信のモック
 		MockRestServiceServer mockServer = MockRestServiceServer.bindTo(restTemplate).ignoreExpectOrder(true)
 				.bufferContent().build();
 		mockServer.expect(method(HttpMethod.POST)).andRespond(withStatus(HttpStatus.FORBIDDEN).body(jsonResponseBody));
-		// 実行
+		// 実行・比較
 		mockMvc.perform(MockMvcRequestBuilders.post("/face/emotion").contentType(MediaType.APPLICATION_JSON)
 				.content("{\"url\":\"https://nishikawa.blob.core.windows.net/images/sugimoto/2020/11/01/01.jpg\"}"))
-				.andDo(print())
+				.andExpect(content().json(expect))
 				.andExpect(status().isInternalServerError());
 
 		mockServer.verify();
@@ -186,18 +205,19 @@ class FaceEmotionControllerTest {
 	// レスポンス HTTPステータス503を期待
 	@Test
 	void testAnalyzeReturn503() throws Exception {
-
-		// 返却されるレスポンス
+		// FaceAPIからのレスポンス
 		String jsonResponseBody = "{ \"error\": { \"code\": \"BadArgument\", \"message\": \"Request body is invalid.\"} }";
+		// 期待値
+		String expect = "{\"error\":\"Face API server unavalable.\",\"details\":{\"code\":\"BadArgument\",\"message\":\"Request body is invalid.\"}}";
 		// FaceAPI通信のモック
 		MockRestServiceServer mockServer = MockRestServiceServer.bindTo(restTemplate).ignoreExpectOrder(true)
 				.bufferContent().build();
 		mockServer.expect(method(HttpMethod.POST))
 				.andRespond(withStatus(HttpStatus.SERVICE_UNAVAILABLE).body(jsonResponseBody));
-		// 実行
+		// 実行・比較
 		mockMvc.perform(MockMvcRequestBuilders.post("/face/emotion").contentType(MediaType.APPLICATION_JSON)
 				.content("{\"url\":\"https://nishikawa.blob.core.windows.net/images/sugimoto/2020/11/01/01.jpg\"}"))
-				.andDo(print())
+				.andExpect(content().json(expect))
 				.andExpect(status().isServiceUnavailable());
 
 		mockServer.verify();
--- server/FaceEmotionAnalyzerAPI/src/test/java/com/face/response/ResponseFactoryTest.java
+++ server/FaceEmotionAnalyzerAPI/src/test/java/com/face/response/ResponseFactoryTest.java
@@ -9,8 +9,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.face.model.Emotion;
 import com.face.model.ErrorResponse;
 import com.face.model.FaceApiErrorResponse;
+import com.face.model.FaceAttributes;
+import com.face.model.FaceRectangle;
+import com.face.model.MeanFaceAttributes;
+import com.face.model.ResponseData;
+import com.face.model.ResultData;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 
@@ -25,9 +31,9 @@ class ResponseFactoryTest {
 	// ErrorResponseが生成され、そのerrorの値が"test error"であることを期待
 	@Test
 	void testCreateErrorResponse() {
-		
+
 		String expectedErrorMessage = "test error"; // このAPIから返されるエラー文
-		
+
 		Exception ex = new NotDetectedException(expectedErrorMessage);
 		// 実行
 		ErrorResponse response = ResponseFactory.createErrorResponse(ex);
@@ -40,12 +46,13 @@ class ResponseFactoryTest {
 	// FaceApiErrorResponse.detailのcode, messageにSON文字列で指定されたエラーの内容が格納
 	@Test
 	void testCreateFaceApiErrorResponse() throws JsonMappingException, JsonProcessingException {
-		
+
 		String expectedErrorMessage = "Face API response is error."; // このAPIから返されるエラー文
 		String expetedDetailCode = "BadArgument"; // FaceAPIからのレスポンス
 		String expectedDetailMessage = "Request body is invalid."; // FaceAPIからのレスポンス
-		
-		String FaceApiErrMessage = "{\"error\":{\"code\":\"" + expetedDetailCode + "\",\"message\":\"" + expectedDetailMessage + "\"}}"; // FaceAPIから返却されるエラーの想定
+
+		String FaceApiErrMessage = "{\"error\":{\"code\":\"" + expetedDetailCode + "\",\"message\":\""
+				+ expectedDetailMessage + "\"}}"; // FaceAPIから返却されるエラーの想定
 		Exception ex = new FaceApiException(FaceApiErrMessage);
 		// 実行
 		FaceApiErrorResponse response = ResponseFactory.createFaceApiErrorResponse(ex);
@@ -53,6 +60,183 @@ class ResponseFactoryTest {
 		assertThat(response.getMessage()).isEqualTo(expectedErrorMessage);
 		assertThat(response.getDetails().getCode()).isEqualTo(expetedDetailCode);
 		assertThat(response.getDetails().getMessage()).isEqualTo(expectedDetailMessage);
+	}
+
+	// ===== floorDecimal のテスト =====
+	
+	// 小数第4位切り捨て
+	@Test
+	void testFloorDecimal1() {
+		// 期待値
+		double expect = 0.123;
+		// 実行
+		double result = ResponseFactory.floorDecimal(0.1234, 4);
+		// 比較
+		assertThat(result).isEqualTo(expect);
+	}
+
+	// 小数第４位切り捨て(5以上の数字の場合)
+	@Test
+	void testFloorDecimal2() {
+		// 期待値
+		double expect = 0.123;
+		// 実行
+		double result = ResponseFactory.floorDecimal(0.1236, 4);
+		// 比較
+		assertThat(result).isEqualTo(expect);
+	}
+
+	// 元の数字が小数第1位までの場合、小数第4位を指定しての切り捨て
+	@Test
+	void testFloorDecimal3() {
+		// 期待値
+		double expect = 0.1;
+		// 実行
+		double result = ResponseFactory.floorDecimal(0.1, 4);
+		// 比較
+		assertThat(result).isEqualTo(expect);
+	}
+
+	// 小数第3位以下の切り捨て
+	@Test
+	void testFloorDecimal4() {
+		// 期待値
+		double expect = 0.12;
+		// 実行
+		double result = ResponseFactory.floorDecimal(0.1234, 3);
+		// 比較
+		assertThat(result).isEqualTo(expect);
+	}
+
+	// 小数点以下の切り捨て
+	@Test
+	void testFloorDecimal5() {
+		// 期待値
+		double expect = 1;
+		// 実行
+		double result = ResponseFactory.floorDecimal(1.1234, 1);
+		// 比較
+		assertThat(result).isEqualTo(expect);
+	}
+	
+	// ===== calcMeanEmotion のテスト =====
+	
+	// 複数の分析結果を含む配列resultDataのemotionの平均値が格納された
+	// Emotionオブジェクトが返却される
+	@Test
+	void testCalcMeanEmotion() {
+		// 期待値
+			// 分析結果を作成
+		Emotion emotion1 = new Emotion();
+		emotion1.setAnger(0.1);
+		emotion1.setContempt(0.1);
+		emotion1.setDisgust(0.1);
+		emotion1.setFear(0.1);
+		emotion1.setHappiness(0.1);
+		emotion1.setNeutral(0.1);
+		emotion1.setSadness(0.1);
+		emotion1.setSurprise(0.1);
+		FaceAttributes faceAttributes1 = new FaceAttributes();
+		faceAttributes1.setEmotion(emotion1);
+		ResultData resultData1 = new ResultData();
+		resultData1.setFaceAttributes(faceAttributes1);
+		Emotion emotion2 = new Emotion();
+		emotion2.setAnger(0.2);
+		emotion2.setContempt(0.2);
+		emotion2.setDisgust(0.2);
+		emotion2.setFear(0.2);
+		emotion2.setHappiness(0.2);
+		emotion2.setNeutral(0.2);
+		emotion2.setSadness(0.2);
+		emotion2.setSurprise(0.2);
+		FaceAttributes faceAttributes2 = new FaceAttributes();
+		faceAttributes2.setEmotion(emotion2);
+		ResultData resultData2 = new ResultData();
+		resultData2.setFaceAttributes(faceAttributes2);
+		ResultData[] resultDatas = {resultData1, resultData2};
+			// emotionの平均値
+		Emotion meanEmotion = new Emotion();
+		meanEmotion.setAnger(0.15);
+		meanEmotion.setContempt(0.15);
+		meanEmotion.setDisgust(0.15);
+		meanEmotion.setFear(0.15);
+		meanEmotion.setHappiness(0.15);
+		meanEmotion.setNeutral(0.15);
+		meanEmotion.setSadness(0.15);
+		meanEmotion.setSurprise(0.15);
+		FaceAttributes meanFaceAttributes = new FaceAttributes();
+		meanFaceAttributes.setEmotion(meanEmotion);
+		MeanFaceAttributes mean = new MeanFaceAttributes();
+		mean.setFaceAttributes(faceAttributes2);
+		
+		ResponseData expect = new ResponseData();
+		expect.setTotal(2);
+		expect.setMean(mean);
+		expect.setResultData(resultDatas);
+		
+		// 実行
+		Emotion result = ResponseFactory.calcMeanEmotion(resultDatas);
+		// 比較
+		assertThat(result.getAnger()).isEqualTo(meanEmotion.getAnger());
+		assertThat(result.getContempt()).isEqualTo(meanEmotion.getContempt());
+		assertThat(result.getDisgust()).isEqualTo(meanEmotion.getDisgust());
+		assertThat(result.getFear()).isEqualTo(meanEmotion.getFear());
+		assertThat(result.getHappiness()).isEqualTo(meanEmotion.getHappiness());
+		assertThat(result.getNeutral()).isEqualTo(meanEmotion.getNeutral());
+		assertThat(result.getSadness()).isEqualTo(meanEmotion.getSadness());
+		assertThat(result.getSurprise()).isEqualTo(meanEmotion.getSurprise());
+
+	}
+	
+	// ===== createSuccessResponse のテスト =====
+
+	// 返却レスポンスが生成される
+	@Test
+	void testCreateSuccessResponse() {
+		// 期待値作成
+		Emotion emotion1 = new Emotion();
+		emotion1.setAnger(0.1);
+		emotion1.setContempt(0.1);
+		emotion1.setDisgust(0.1);
+		emotion1.setFear(0.1);
+		emotion1.setHappiness(0.1);
+		emotion1.setNeutral(0.1);
+		emotion1.setSadness(0.1);
+		emotion1.setSurprise(0.1);
+		FaceAttributes faceAttributes1 = new FaceAttributes();
+		faceAttributes1.setEmotion(emotion1);
+		FaceRectangle faceRectangle1 = new FaceRectangle();
+		faceRectangle1.setTop(10);
+		faceRectangle1.setLeft(20);
+		faceRectangle1.setWidth(150);
+		faceRectangle1.setHeight(120);
+		ResultData resultData1 = new ResultData();
+		resultData1.setFaceId("01");
+		resultData1.setFaceRectangle(faceRectangle1);
+		resultData1.setFaceAttributes(faceAttributes1);
+		MeanFaceAttributes mean = new MeanFaceAttributes();
+		mean.setFaceAttributes(faceAttributes1);
+		ResultData[] list = { resultData1 };
+
+		ResponseData responseData = new ResponseData();
+		responseData.setTotal(1);
+		responseData.setResultData(list);
+		responseData.setMean(mean);
+
+		// 実行
+		ResponseData result = ResponseFactory.createSuccessResponse(list);
+
+		// 比較
+		assertThat(result.getTotal()).isEqualTo(responseData.getTotal());
+		assertThat(result.getMean().getFaceAttributes().getEmotion().getAnger()).isEqualTo(responseData.getMean().getFaceAttributes().getEmotion().getAnger());
+		assertThat(result.getMean().getFaceAttributes().getEmotion().getContempt()).isEqualTo(responseData.getMean().getFaceAttributes().getEmotion().getAnger());
+		assertThat(result.getMean().getFaceAttributes().getEmotion().getDisgust()).isEqualTo(responseData.getMean().getFaceAttributes().getEmotion().getAnger());
+		assertThat(result.getMean().getFaceAttributes().getEmotion().getFear()).isEqualTo(responseData.getMean().getFaceAttributes().getEmotion().getAnger());
+		assertThat(result.getMean().getFaceAttributes().getEmotion().getHappiness()).isEqualTo(responseData.getMean().getFaceAttributes().getEmotion().getAnger());
+		assertThat(result.getMean().getFaceAttributes().getEmotion().getNeutral()).isEqualTo(responseData.getMean().getFaceAttributes().getEmotion().getAnger());
+		assertThat(result.getMean().getFaceAttributes().getEmotion().getSadness()).isEqualTo(responseData.getMean().getFaceAttributes().getEmotion().getAnger());
+		assertThat(result.getMean().getFaceAttributes().getEmotion().getSurprise()).isEqualTo(responseData.getMean().getFaceAttributes().getEmotion().getAnger());
+
 	}
 
 }
